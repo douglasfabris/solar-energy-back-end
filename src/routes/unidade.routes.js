13c3bc1e143afaea63c00a4acb3469bbadcdc587
douglas <douglas.df1@gmail.com>
2023-09-13 21:07:33 -0300

    autenticacao jwt

--- src/routes/unidade.routes.js
+++ src/routes/unidade.routes.js
@@ -4,16 +4,16 @@ const {
   atualizarUnidade,
   excluirUnidade,
 } = require("../controllers/unidade.controller");
-
+const { auth } = require("../middleware/auth");
 const { Router } = require("express");
 
 class UnidadeRouter {
   routesFromUnidade() {
     const unidadeRoutes = Router();
-    unidadeRoutes.get("/unidade", listarUnidades);
-    unidadeRoutes.post("/unidade", criarUnidade);
-    unidadeRoutes.put("/unidade/:id", atualizarUnidade);
-    unidadeRoutes.delete("/unidade/:id", excluirUnidade);
+    unidadeRoutes.get("/unidade", auth, listarUnidades);
+    unidadeRoutes.post("/unidade", auth, criarUnidade);
+    unidadeRoutes.put("/unidade/:id", auth, atualizarUnidade);
+    unidadeRoutes.delete("/unidade/:id", auth, excluirUnidade);
     return unidadeRoutes;
   }
 }
