cec7feb0425fb863fc0017995beee2e7a7522c23
douglas <douglas.df1@gmail.com>
2023-09-14 20:04:55 -0300

    main funcional

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const { routesFromUnidade } = require("./unidade.routes");
 
 // Definição da rota base da api, chamando também as rotas de cada tabela
 const routes = new Router();
-routes.use("/api/v1", [routesFromUsuario(), routesFromUnidade()]]);
-
+routes.use("/api/v1", [routesFromUsuario(), routesFromUnidade()]);
 
 module.exports = routes;
